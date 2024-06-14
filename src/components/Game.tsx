16eba34587f3fcb62f3355278aaf4054aa917262
hrysdev <hrysdev@gmail.com>
2024-06-15 00:16:02 +0900

    feat: Gameコンポーネントを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Board from "./Board";
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [histtory, setHistory] = useState([Array(9).fill(null)]);
+
   return (
     <div className="game">
       <div className="game-board">
