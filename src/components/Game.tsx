31038aae50315847ad9587a56e49f4dbd77e1d76
hrysdev <hrysdev@gmail.com>
2024-06-15 00:34:11 +0900

    feat: 現在の手を履歴で管理するように修正
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import Board from "./Board";
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [histtory, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [history, setHistory] = useState<Array<Array<string>>>([
+    Array(9).fill(null),
+  ]);
+  const currentSquares = history[history.length - 1];
+
+  const handlePlay = (nextSquares: string[]) => {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  };
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/* TODO */}</ol>
